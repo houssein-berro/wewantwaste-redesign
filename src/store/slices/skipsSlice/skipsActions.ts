916a9ed469321a62750720bcb5c9fdcd8f10abad
Berro-1 <hussienberro1@gmail.com>
2025-06-05 08:07:45 +0300

    Created a skipsActions file which holds the function to load the skips from ( api/localstorage/redux store)

---
+++ src/store/slices/skipsSlice/skipsActions.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import {
+  fetchSkipsStart,
+  fetchSkipsSuccess,
+  fetchSkipsFailure,
+} from "./skipsSlice";
+import type { Skip } from "../../../constants/skips";
+import { SKIPS_API_URL, LOCAL_STORAGE_KEY } from "../../../constants/skips";
+import type { AppDispatch } from "../../store";
+
+
+export async function loadSkips(dispatch: AppDispatch) {
+  let cachedData: Skip[] | null = null;
+
+  const stored = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (stored) {
+    try {
+      cachedData = JSON.parse(stored) as Skip[];
+      dispatch(fetchSkipsSuccess(cachedData));
+      
+    } catch {
+      cachedData = null;
+    }
+  }
+
+  if (!cachedData) {
+    dispatch(fetchSkipsStart());
+  }
+
+  try {
+    const response = await axios.get<Skip[]>(SKIPS_API_URL);
+    const apiData = response.data;
+
+    const jsonApi = JSON.stringify(apiData);
+    const jsonCached = cachedData ? JSON.stringify(cachedData) : null;
+
+    if (jsonApi !== jsonCached) {
+      try {
+        localStorage.setItem(LOCAL_STORAGE_KEY, jsonApi);
+      } catch {
+        
+      }
+      dispatch(fetchSkipsSuccess(apiData));
+    }
+  } catch (err: any) {
+    if (!cachedData) {
+      let message = "Unknown error";
+      if (axios.isAxiosError(err)) {
+        message = err.response?.data
+          ? JSON.stringify(err.response.data)
+          : err.message;
+      } else if (err instanceof Error) {
+        message = err.message;
+      }
+      dispatch(fetchSkipsFailure(message));
+    }
+  }
+}
