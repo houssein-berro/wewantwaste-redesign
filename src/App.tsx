a156dd142f83b7913eac602a905eb61cca06d71c
Berro-1 <hussienberro1@gmail.com>
2025-06-05 08:05:59 +0300

    Added a provider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import SkipSizePage from "./pages/skipSizePage";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 export default function App() {
-  return <SkipSizePage />;
+
+  return (
+    <Provider store={store}>
+      <SkipSizePage />;
+    </Provider>
+  );
 }
